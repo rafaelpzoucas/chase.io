1e12b3c5ce2f0c2d80867fd82f656512c0593f34
Rafael Zoucas <rafaelpzoucas@gmail.com>
2025-09-09 21:35:57 -0300

    test: bug on immune color

--- src/utils/getPlayerColors.ts
+++ src/utils/getPlayerColors.ts
@@ -7,12 +7,17 @@ export function getPlayerColor(
 ): string {
   if (player.isIt) return PLAYER_COLORS.PIQUE;
 
+  console.log({
+    immuneUntil: player.immuneUntil,
+    dateNow: Date.now(),
+    isImmune: Date.now() < player.immuneUntil,
+  });
+
   if (player.immuneUntil && Date.now() < player.immuneUntil) {
     return PLAYER_COLORS.IMMUNE;
   }
 
   if (isCurrentPlayer) return PLAYER_COLORS.SELECTED;
 
-  // pode até separar se for currentPlayer ou não
   return PLAYER_COLORS.NORMAL;
 }
